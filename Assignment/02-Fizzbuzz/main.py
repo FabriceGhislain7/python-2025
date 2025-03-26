f342f6d16f87e286de86bb13e30f234a3c108b2d
delectablerec <info@delectablerecords.com>
2025-03-26 10:19:27 +0100

    fizzbuzz v 1

--- Assignment/02-Fizzbuzz/main.py
+++ Assignment/02-Fizzbuzz/main.py
@@ -1,4 +1,4 @@
-numero = int(input("Inserisci un numero: "))
+numero = int(input("Inserisci un numero: "))  # effettuo la conversione in intero del valore inserito dall'utente in modo da poter effettuare i controlli successivi
 if numero % 3 == 0 and numero % 5 == 0:
     print("FizzBuzz")
 elif numero % 3 == 0:
