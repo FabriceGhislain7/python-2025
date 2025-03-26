87a579ae22efdc772dde8f08dca7a2b10408c322
delectablerec <info@delectablerecords.com>
2025-03-26 09:59:28 +0100

    fizzbuzz v 2

--- Assignment/02-Fizzbuzz/main.py
+++ Assignment/02-Fizzbuzz/main.py
@@ -1,10 +1,9 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        # faccio prima il fizzbuzz perche' se metto prima il fizz o il buzz non entra mai nella condizione dove i e' multiplo di 3 e 5
-        print(f"{i} -> FizzBuzz")
-    elif i % 3 == 0:
-        print(f"{i} -> Fizz")
-    elif i % 5 == 0:
-        print(f"{i} -> Buzz")
-    else:
-        print(i)
+numero = int(input("Inserisci un numero: "))
+if numero % 3 == 0 and numero % 5 == 0:
+    print("FizzBuzz")
+elif numero % 3 == 0:
+    print("Fizz")
+elif numero % 5 == 0:
+    print("Buzz")
+else:
+    print(numero)
