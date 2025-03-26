2b739a4f47bda07d49febc741d3868b7ba6721ab
delectablerec <ghislaintebou@gmail.com>
2025-03-26 11:29:29 +0100

    seconda e terza  versione del codice FIZZBUZZ.

--- esercitazioni/Assignment/FIZZBUZZ/main.py
+++ esercitazioni/Assignment/FIZZBUZZ/main.py
@@ -1,11 +1,22 @@
 
+while True:
+    inserimento = input("Inserisci un numero intero valido oppure inserici 'esci' per uscire dal programma: ")
+    
+    if inserimento.lower() == "esci":
+        print("Ciao, alla prossima.")
+        break
+    
+    if not inserimento.isdigit:
+        print("Per favore inserire un numero intero valido oppure 'esci' per uscire dal programma: " )
+        continue # Ricomincia il ciclo for while dall'inizio 
+    
+    numero = int(inserimento)    # Convertiamo la striga in numer intero.
 
-for numero in range(1, 101): # Per ogin numero da 1 a 100
-    if numero % 3 == 0 and numero % 5 == 0: # Se è divisibile per entrambi, stampa "FizzBuzz".
-        print("FizzBuzz")
-    elif numero % 3 == 0: # Se è divisibile per 5, stampa "Buzz"
-        print("Fizz")
-    elif numero % 5 == 0: # Se è divisibile per entrambi, stampa "FizzBuzz".
-        print("Buzz")
-    else: # Altrimenti, stampa il numero
+    if numero % 3 == 0 and numero % 5 == 0: 
+        print(f"{numero} -> FizzBuzz")
+    elif numero % 3 == 0: 
+        print(f"{numero} -> Fizz")
+    elif numero % 5 == 0: 
+        print(f"{numero} -> Buzz")
+    else: 
         print(numero)
